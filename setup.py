269511160ab971421b02d6bddb97840a172724e6
j08lue <j08lue@gmail.com>
2014-02-17 08:54:52 +0100

    fixed dependency_links

--- setup.py
+++ setup.py
@@ -12,6 +12,16 @@ setup(name = 'oceanpy',
       license = 'LICENSE.txt',
       description = 'Physical Oceanography for Python',
       long_description = open('README.md').read(),
-      install_requires = ['prettycpt'],
-      dependency_links = ['https://github.com/j08lue/prettycpt/tarball/master#egg=package-1.0']
+      install_requires = [
+          'pygeo',
+          'pycpt',
+          'prettycpt',
+          'numpy',
+          'matplotlib',
+          ],
+      dependency_links = [
+          'https://github.com/j08lue/pygeo/tarball/master#egg=pygeo-0.1.0',
+          'https://github.com/j08lue/pycpt/tarball/master#egg=pycpt-0.1.0',
+          'https://github.com/j08lue/prettycpt/tarball/master#egg=prettycpt-0.1.0',
+          ]
       )
